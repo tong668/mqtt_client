7f4550a814f1ed8fc20abfd4d68415756322eb3d
tong668 <tongzhenyu86@126.com>
2022-11-02 18:13:13 +0800

    update

--- sample/mqtt_pub.c
+++ sample/mqtt_pub.c
@@ -27,6 +27,8 @@ int main(int argc, char* argv[])
 
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
+    conn_opts.MQTTVersion = MQTTVERSION_3_1_1;
+
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
     {
         printf("Failed to connect, return code %d\n", rc);
